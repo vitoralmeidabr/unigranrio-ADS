d88f18c036f3e6ddd8f63bcbfeceed0564de8aa5
Vitor Almeida <cadastros@vitoralmeida.com.br>
2023-03-18 13:17:15 -0300

    feat(2023-1): Praticas de programação
    AP1: Exercícios 1,2 e 7

---
+++ 2023-1/praticas-programacao/AP1/exercicio-2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import math
+
+print("Vamos calcular a média das suas notas bimestrais?")
+print("Exemplo: 8.5 / 4 / 0 / 10")
+
+while True:
+    print("")
+    nota1 = input("Informe a primeira nota: ")
+    nota2 = input("Informe a segunda nota: ")
+    nota3 = input("Informe a terceira nota: ")
+    nota4 = input("Informe a quarta nota: ")
+    
+    print("")
+    try:
+        nota1 = float(nota1.replace(",","."))
+        nota2 = float(nota2.replace(",","."))
+        nota3 = float(nota3.replace(",","."))
+        nota4 = float(nota4.replace(",","."))       
+        
+        mediaFinal = round((nota1 + nota2 + nota3 + nota4)/4, 1)
+    except ValueError:
+        print("Verifique o valor informado, para calcular a média das notas você deve informar apenas números.")
+        print("Vamos tentar de novo") 
+        continue
+    
+    if ((nota1<0 or nota1 >10) or (nota2<0 or nota2 >10) or (nota3<0 or nota3 >10) or (nota4<0 or nota4 >10)):
+        print("As notas devem ser entre 0 e 10 e com apenas um dígito decimal, por exemplo \"0\" ou \"8.5\", OK?")
+        print("Tente novamente.")
+    else:
+        print("OK, calculando a média final das 4 notas bimestrais...") # % (nota1,nota2,nota3,nota4) 
+        print("\nResultado:")
+        
+        print("\nMédia final: ",end="")
+        print(mediaFinal)	
+        
+        print("\nVamos continuar? Podemos tentar com outras notas.")
+
