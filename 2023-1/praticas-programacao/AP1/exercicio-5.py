0106f6103f49a839a2d828760871490b3fa2d758
Vitor Almeida <cadastros@vitoralmeida.com.br>
2023-04-03 01:37:55 -0300

    feat: atualizando exercício 2 e novo ex 5

--- 2023-1/praticas-programacao/AP1/exercicio-5.py
+++ 2023-1/praticas-programacao/AP1/exercicio-5.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import math
 
-print("Vamos calcular a média das suas notas bimestrais?")
+print("Vamos calcular a média das suas notas e saber se você foi aprovado?")
 
 while True:
     print("")
@@ -9,32 +8,32 @@ while True:
 
     nota1 = input("Informe a primeira nota: ")
     nota2 = input("Informe a segunda nota: ")
-    nota3 = input("Informe a terceira nota: ")
-    nota4 = input("Informe a quarta nota: ")
-    
+
     print("")
     try:
         nota1 = float(nota1.replace(",","."))
         nota2 = float(nota2.replace(",","."))
-        nota3 = float(nota3.replace(",","."))
-        nota4 = float(nota4.replace(",","."))       
         
-        mediaFinal = round((nota1 + nota2 + nota3 + nota4)/4, 1)
+        mediaFinal = round((nota1 + nota2)/2, 1)
     except ValueError:
         print("Verifique o valor informado, para calcular a média das notas você deve informar apenas números conforme o exemplo.")
         print("Vamos tentar de novo") 
         continue
-    
-    if ((nota1<0 or nota1 >10) or (nota2<0 or nota2 >10) or (nota3<0 or nota3 >10) or (nota4<0 or nota4 >10)):
+
+    if ((nota1<0 or nota1 >10) or (nota2<0 or nota2 >10)):
         print("As notas devem ser entre 0 e 10 e com apenas um dígito decimal, por exemplo \"0\" ou \"8.5\", OK?")
         print("Tente novamente.")
     else:
-        print("OK, calculando a média final das 4 notas bimestrais...") # % (nota1,nota2,nota3,nota4) 
+        print("OK, calculando a média final...")
         print("\nResultado:")
-        
-        print("\nMédia final: ",end="")
-        print(mediaFinal)	
-        print("\nMédia final: %s" % mediaFinal)
-        
-        print("\nVamos continuar? Podemos tentar com outras notas.")
 
+        print("\nMédia final: %s" % mediaFinal)
+
+        if mediaFinal == 10:
+            print('Parabéns, você foi aprovado com distinção!')
+        elif 10 > mediaFinal >= 7:
+            print('Você foi aprovado.')
+        else:
+            print('Infelizmente você foi reprovado.')
+
+        print("\nVamos continuar? Podemos tentar com outras notas.")
